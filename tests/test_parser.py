ed1095f578da4c9c4f7aca0015513ccc1fff4e79
Robert Nyström <golgafrincham@gmail.com>
2020-06-05 19:39:21 +0200

    added tests

---
+++ tests/test_parser.py
@@ -0,0 +1,17 @@
+import test_importer
+import parser
+
+
+def test_list_data_files():
+    # Initiate - Create folder structures and files, manually create list with files.
+    # Run tests - Run list_data files
+    # Evaluate tests - assert x=y (List with file names match)
+    assert 1 == 0
+
+
+def test_convert_to_int():
+    assert 1 == 0
+
+
+def test_read_data():
+    assert 1 == 0
